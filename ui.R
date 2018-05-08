acae65a5f21d06bc30c51ce3acbeb553ad5b9d76
Paul Campbell <pacampbell91@gmail.com>
2018-05-08 16:47:16 +0100

    coupla edits

--- ui.R
+++ ui.R
@@ -1,30 +1,10 @@
 library(shiny)
 library(shinydashboard)
 
-url <- "https://twitter.com/intent/tweet"
-
 dashboardPage(
   
   dashboardHeader(
-    title = "Reactive Module Calls",
-    tags$li(
-      class = "dropdown",
-      style="padding: 10px;",
-      HTML(sprintf('<a 
-                  href=%s 
-                  class="twitter-share-button"
-                  data-size="large"
-                  data-text="Reactively load and call modules in R Shiny apps"
-                  data-url="https://cultureofinsight.com/"
-                  data-hashtags="rstats,rshiny"
-                  data-via="paulcampbell91">
-                  Tweet
-                  </a>', 
-                   url)),
-      # Copy the script from https://dev.twitter.com/web/javascript/loading into your app
-      # You can source it from the URL below. It must go after you've created your link
-      includeScript("http://platform.twitter.com/widgets.js")
-    )
+    title = "Reactive Module Calls"
   ),
   
   dashboardSidebar(
@@ -36,7 +16,6 @@ dashboardPage(
     tags$hr(),
     
     uiOutput("globalenv")
-    
   ),
   
   dashboardBody(
